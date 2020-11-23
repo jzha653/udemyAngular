33ad286b8a7a3cf385eaa37b829bec5034c1714b
Jiaqiu Zhao <jzha653@aucklanduni.ac.nz>
2020-11-23 22:02:27 +1300

    #22

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  selector: '[app-servers]',
+  template: `<app-server></app-server>
+  <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
