0271cda8657fe6d7b59c8bb2a472e2c82c39b0c3
Jiaqiu Zhao <jzha653@aucklanduni.ac.nz>
2020-11-23 21:21:37 +1300

    initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/firt-app'),
+      dir: require('path').join(__dirname, './coverage/udemy-angular-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
